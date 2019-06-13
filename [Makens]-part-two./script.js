360fc932740db3109209de8c78b9c215ec081380
J-Makens <51523858+J-Makens@users.noreply.github.com>
2019-06-13 05:10:22 -0400

    Add files via upload

---
+++ [Makens]-part-two./script.js
@@ -0,0 +1,68 @@
+
+var name = 'eventType';
+var name = 'tempFahr';
+console.log('eventType' + 'tempFahr');
+
+<script>
+{
+var x = prompt ("casual": "something comfy");
+var x = prompt ("semi-formal": "a polo");
+var x = prompt ("formal " : "a suit");
+x++;
+}
+</script>
+
+
+// eventType prompt: pick from casual, semi-formal, formal
+// tempFahr: prompt for less than 54 degrees Fahrenheit, 54-70 degrees Fahrenheit, more than 70 degrees Fahrenheit
+// Result: the string that will be logged to the console: single string using concatenation. Containing eventTyep, temp, clothing suggestion: 
+// ex. "Since it is 33 degrees and you are going to a formal event, you should wear a suit and coat.
+
+
+
+ var y <= 0;
+   while (y <=100) {
+       console.log (y);
+       y++;
+   }
+
+
+// If event type = casual; clothing suggestion = something comfy;
+// If event type = semi-formal; clothing suggestion = a polo;
+// If event type = formal; clothing suggestion = suit;
+
+var data = {
+    coat : true,
+}
+for (var y<=54 , y>=0 ; element --)
+console.log (var y);
+
+var data = {
+a jacket : true,
+}
+for (var y<=54 , var y>70; element --)
+console.log (var y);
+
+var data = {
+no jacket : true,
+}
+for (var yr>= 70, var y< 100; element --)
+console.log (var y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
